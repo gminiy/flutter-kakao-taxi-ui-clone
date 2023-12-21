d89dca12e517e45802a698b6a6373045963e7e3c
gminiy <gmini.y@gmail.com>
2023-12-21 20:29:20 +0900

    Feat: HomeScreen 아이콘 배치

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_taxi_ui_clone/widget/icon_bottom_label.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,11 +7,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
       width: double.infinity,
-      child: Column(
+      child: const Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('icons'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconBottomLabel(
+                image: 'assets/taxi.png',
+                label: '택시',
+              ),
+              IconBottomLabel(
+                image: 'assets/taxi_black.png',
+                label: '블랙',
+              ),
+              IconBottomLabel(
+                image: 'assets/bicycle.png',
+                label: '바이크',
+              ),
+              IconBottomLabel(
+                image: 'assets/steering_wheel.png',
+                label: '카풀',
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconBottomLabel(
+                image: 'assets/parking.png',
+                label: '주차',
+              ),
+              IconBottomLabel(
+                image: 'assets/car_pool.png',
+                label: '카풀',
+              ),
+              IconBottomLabel(
+                image: 'assets/navigation.png',
+                label: '네비',
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: IconBottomLabel(
+                  image: 'assets/navigation.png',
+                  label: '투명한 아이콘',
+                ),
+              ),
+            ],
+          ),
           Text('banners'),
           Text('notification'),
         ],
