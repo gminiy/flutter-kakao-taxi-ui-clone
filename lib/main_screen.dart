ab2c9695398b0283b49c0a5e8aeeb5de986d3e12
gminiy <gmini.y@gmail.com>
2023-12-21 16:11:46 +0900

    Feat: bottomNavigationBar 생성

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +23,35 @@ class MainScreen extends StatelessWidget {
           color: Colors.black,
           fontSize: 18,
           fontWeight: FontWeight.bold,
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: (index) {
+            setState(() {
+              _index = index;
+            });
+          },
+          currentIndex: _index,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              label: '홈',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: '이용 서비스',
+              icon: Icon(Icons.assignment),
+            ),
+            BottomNavigationBarItem(
+              label: '내 정보',
+              icon: Icon(Icons.account_box),
+            ),
+          ],
         ),
       ),
     );
