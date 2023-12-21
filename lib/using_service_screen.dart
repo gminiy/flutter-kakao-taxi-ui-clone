3752add0090c5fd3e0fef412dcafafdbc51efcfa
gminiy <gmini.y@gmail.com>
2023-12-21 16:19:40 +0900

    Feat: Home, MyInfo, UserService 기본 페이지 생성

---
+++ lib/using_service_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UsingServiceScreen extends StatelessWidget {
+  const UsingServiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('이용 서비스'),
+    );
+  }
+}
